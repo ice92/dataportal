cc244d7f615a731adc7efc8aaa24214dfe14155d
Imam Arief Putrajaya <ice.codename@gmail.com>
2021-10-13 12:29:41 +0800

    finish project

---
+++ application/views/home.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Data Portal</title>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <!-- Bootstrap core CSS -->
+        <link href="<?php echo base_url();?>/assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="<?php echo base_url();?>/assets/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+<!--Main Navigation-->
+<header>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark default-color double-nav">
+    <!-- SideNav slide-out button -->
+    <div class="float-left">
+        <a href="#" data-activates="slide-out" class="button-collapse">
+            <i class="fas fa-bars"></i>
+        </a>
+    </div>
+    <!-- Breadcrumb -->
+    <div class="breadcrumb-dn mr-auto">
+        <p> Material Design for Bootstrap</p>
+    </div>
+    <!-- Links -->
+    <ul class="nav navbar-nav nav-flex-icons ml-auto">
+        <li class="nav-item">
+            <a class="nav-link">
+                <i class="fas fa-envelope"></i>
+                <span class="clearfix d-none d-sm-inline-block">Contact</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">
+                <i class="fas fa-cog"></i>
+                <span class="clearfix d-none d-sm-inline-block">Settings</span>
+            </a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+                <span class="clearfix d-none d-sm-inline-block">Account</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+    </ul>
+
+</nav>
+<!--/.Navbar-->
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main>
+
+</main>
+<!--Main layout-->
+
+<!--Footer-->
+<footer class="page-footer font-small default-color">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!--Footer-->
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="<?php echo base_url();?>/assets/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="<?php echo base_url();?>/assets/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="<?php echo base_url();?>/assets/js/mdb.min.js"></script>
+</body>
+</html>
